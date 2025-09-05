4342d1ba9151fe3355a4ebbd347e8b4f9e6c423f
Robert Starsi <klevo@klevo.sk>
2025-09-05 10:48:03 +0200

    projects_file_path

--- lib/locport.rb
+++ lib/locport.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require "thor"
+require "fileutils"
+
+APP_NAME = "locport"
+DATA_FILE = "projects"
 
 module Locport
   class Main < Thor
@@ -11,6 +15,31 @@ module Locport
     desc "index [PATH]", "Index a project"
     def index(path = Dir.pwd)
       say "Indexing #{path}"
+      say projects_file_path
     end
+
+    private
+      def storage_base_dir
+        if Gem.win_platform?
+          ENV["APPDATA"] || File.join(Dir.home, "AppData", "Roaming")
+        else
+          ENV["XDG_DATA_HOME"] || File.join(Dir.home, ".local", "share")
+        end
+      end
+
+      def storage_dir
+        File.join(storage_base_dir, APP_NAME)
+      end
+
+      def projects_file_path
+        FileUtils.mkdir_p(storage_dir)
+        File.join(storage_dir, DATA_FILE)
+      end
+
+      def append_to_projects(line)
+        File.open(projects_file_path, "a") do |f|
+          f.puts(line)
+        end
+      end
   end
 end
